df087426934b3ae359cf4bad1a6d5015b06a5611
Hitoshi Uchida <uchida@ub22.localdomain>
2022-11-15 22:47:12 +0900

    feat(lisp): Enhanced to manage symbols as list to be used to identify the project root besides .git.

--- helm-find-project-files.el
+++ helm-find-project-files.el
@@ -29,25 +29,38 @@
 
 (require 'helm)
 
-(defvar helm-find-project-files-exclude-paths '("*/.git/*" "*.elc") "A list of file patterns to ignore by find command.")
+(defvar helm-find-project-files-ignored-paths '("*/.git/*" "*.elc")
+  "A list of file patterns to ignore with find command.")
+
+(defvar helm-find-project-files-project-root-symbols '(".git" ".svn" ".projectroot")
+  "Symbols to be used to identify the project root.")
+
+(defun helm-find-project-files-project-root-p (dir symbols)
+  "Return t if DIR is a project root directory identified with SYMBOLS."
+  (if symbols
+      (if (file-exists-p (format "%s/%s" dir (car symbols)))
+          t
+        (helm-find-project-files-project-root-p dir (cdr symbols)))))
 
 (defun helm-find-project-files-find-git-dir (dir)
-  "Search up the directory tree looking for a .git directory."
+  "Find the project root directory by recursively searching up the directory tree form DIR."
   (cond
    ((not dir) "")
    ((string= dir "~") "")
    ((string= dir "/") "")
-   ((file-exists-p (format "%s/.git" dir)) dir)
+   ((helm-find-project-files-project-root-p dir helm-find-project-files-project-root-symbols) dir)
    (t (helm-find-project-files-find-git-dir (directory-file-name (file-name-directory dir))))))
 
-(defun helm-find-project-files-construct-find-option (excludes)
-  (if excludes
+(defun helm-find-project-files-construct-find-option (ignored)
+  "Consruct options of find comamnd to exclude directories and files of IGNORED."
+  (if ignored
       (format "-not -path '%s' %s"
-              (car excludes)
-              (helm-find-project-files-construct-find-option (cdr excludes)))
+              (car ignored)
+              (helm-find-project-files-construct-find-option (cdr ignored)))
     ""))
 
 (defun helm-find-project-files-normalize-dir (dir)
+  "Normalize the specified DIR."
   (if (string= (substring dir 0 2) "~/")
       (setq dir (format "%s/%s" (getenv "HOME") (substring dir 2))))
   (if (not (string= (substring dir -1) "/"))
@@ -55,16 +68,18 @@
   dir)
 
 (defun helm-find-project-files-find (dir)
+  "Find project files containing DIR and select by helm inteface."
   (let* ((normalized-dir (helm-find-project-files-normalize-dir dir))
-         (exclude-paths helm-find-project-files-exclude-paths)
-         (exclude-option (helm-find-project-files-construct-find-option exclude-paths))
+         (ignore-option
+          (helm-find-project-files-construct-find-option helm-find-project-files-ignored-paths))
          (hidden-path-len (+ (length normalized-dir) 1))
-         (shell-find-command (format "find %s %s | cut -c %d-" normalized-dir exclude-option hidden-path-len))
+         (shell-find-command
+          (format "find %s %s | cut -c %d-" normalized-dir ignore-option hidden-path-len))
          (fpath-relative (helm :sources
-                    (helm-build-sync-source "helm-find-project-files"
-                      :candidates (split-string
-                                   (shell-command-to-string shell-find-command)
-                                   "\n" t))))
+                               (helm-build-sync-source "helm-find-project-files"
+                                 :candidates (split-string
+                                              (shell-command-to-string shell-find-command)
+                                              "\n" t))))
          (fpath-absolute (format "%s%s" normalized-dir fpath-relative)))
     (if (file-exists-p fpath-absolute)
         (find-file fpath-absolute))))
@@ -72,10 +87,15 @@
 (defun helm-find-project-files ()
   "Search files of the current working project with helm interface.
 
-The root directory of the project is identified by .git by
-searching up from the current file buffer."
+The root directory of the project is identified by searching up
+from the location of the current buffer."
   (interactive)
   (let ((root-dir (helm-find-project-files-find-git-dir default-directory)))
     (if (string= root-dir "")
         (helm-find-project-files-find default-directory)
       (helm-find-project-files-find root-dir))))
+
+
+(provide 'helm-find-project-files)
+
+;;; helm-find-project-files.el ends here
